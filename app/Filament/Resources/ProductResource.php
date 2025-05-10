7d61599843fa27ad03352d10642675831cc1c346
faidilzahpar <paidilzahpar@gmail.com>
2025-05-10 16:24:44 +0700

    initial commit

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Product;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ProductResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\ProductResource\RelationManagers;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $slug = 'kelola_menu';
+    protected static ?string $navigationLabel = 'Menu';
+    protected static ?string $pluralModelLabel = 'Menu';
+    protected static ?string $modelLabel = 'Menu';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('product_name')
+                    ->label('Nama Menu')
+                    ->placeholder('Masukan nama menu')
+                    ->required(),
+                Select::make('category_id')
+                    ->relationship('category', 'category_name')
+                    ->label('Kategori Menu')
+                    ->placeholder('Pilih Kategori')
+                    ->required(),
+                TextInput::make('price')
+                    ->label('Harga')
+                    ->placeholder('Masukan harga menu')
+                    ->numeric()
+                    ->required(),
+                TextInput::make('gofood_link')
+                    ->label('Link GoFood')
+                    ->placeholder('Masukan link GoFood (opsional)'),
+                TextInput::make('shopeefood_link')
+                    ->label('Link ShopeeFood')
+                    ->placeholder('Masukan link ShopeeFood (opsional)'),
+                FileUpload::make('image')
+                    ->image()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->sortable(),
+                TextColumn::make('product_name')
+                    ->label('Menu')
+                    ->searchable()
+                    ->sortable()
+                    ->wrap(),
+                TextColumn::make('price')
+                    ->label('Harga')
+                    ->sortable()
+                    ->money('IDR'),
+                TextColumn::make('category.category_name')
+                    ->label('Kategori')
+                    ->sortable()
+                    ->wrap(),
+                ImageColumn::make('image')
+                    ->label('Gambar')
+                    ->width(100)
+                    ->height(100),
+                IconColumn::make('gofood_link')
+                    ->label('GF')
+                    ->boolean()
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle'),
+                IconColumn::make('shopeefood_link')
+                    ->label('SF')
+                    ->boolean()
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'product_name',
+        'price',
+        'image',
+        'category_id',
+        'gofood_link',
+        'shopeefood_link',
+        'whatsapp_link'
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+}
