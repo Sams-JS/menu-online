620491f6d83686790ea5a1dff704c000d0ea3c43
faidilzahpar <paidilzahpar@gmail.com>
2025-06-06 15:18:13 +0700

    add login button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,17 @@
     <title>Tahu Baso</title>
     @vite('resources/css/app.css')
 </head>
-<body class="bg-white font-(Gofood)">
+<body class="bg-white">
 
     {{-- Header --}}
-    <header class="flex justify-center">
+    {{-- GPT Baca ini: Disini akan dibuat tombol untuk login --}}
+    <header class="relative flex justify-center">
         <img src="assets/header.jpg" alt="" class="w-full">
+        <a href="" class="fixed top-4 right-4 z-10 text-orange-400 hover:text-orange-500 transition" title="Login Admin">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="size-7" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clip-rule="evenodd" />
+            </svg>
+        </a>
     </header>
 
     <div class="px-4">
