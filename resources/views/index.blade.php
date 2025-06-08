785e0f80de3b6939b609648bd5026999089a5c45
faidilzahpar <paidilzahpar@gmail.com>
2025-06-08 11:04:03 +0700

    fix something

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tahu Baso Mas Pendek</title>
     @vite('resources/css/app.css')
-    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-white">
 
