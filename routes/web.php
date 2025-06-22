c82254b12fff613bbfb22c0fd7befb1744dd78bd
faidilzahpar <paidilzahpar@gmail.com>
2025-06-22 12:12:49 +0700

    persingkat kode route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MenuController;
 
-Route::get('/', [MenuController::class, 'index'])->name('home');
+Route::get('/', [MenuController::class, 'index']);
